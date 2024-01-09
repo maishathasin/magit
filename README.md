ba82bc7a06e7a77b7dab474d94abf1416364637a
Maisha Thasin <thasin.maisha@gmail.com>
2024-01-09 06:15:29 -0500

    fix

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # magit
-making git from scratch
+
+This is my minimal recreation of git, currently supports creating a repository, adding files to staging, 
+commiting, creating/switching branches, merging, log and diff. This is only used for learning puposes about the internals of git and is no way should be used for production.
+
+## How to use 
+
+Don't
+
+## Key differences b/w git and magit:
+
+### Staging Area (Index File in Git vs. Staged Area in Magit)
+Git: Uses an 'index' file to track changes that are to be committed. This is a binary file that represents the state of the working directory.
+
+Magit: Manages a 'staged' directory. Each file to be committed is stored here with its content hashed. It's more like a collection of files rather than a  file. 
+
+This has been done for simplicity as git uses a tree structure which is a bit more complicated to implement.
+
+#### Git's Tree Structure
+Blob Objects: The contents of each file are stored as blob objects.
+Tree Objects: Git uses "tree" objects to represent the structure of directories, it contains pointers to blob objects.
+Commits Point to Trees: Each commit in Git points to a tree object
+
+#### Magit's Approach
+Direct Staging: when you add a file in magit, the content of the file is stored directly in the staged directory. 
+
+Commit Structure: the commit object itself stores the changes. This is different from Git wwhere a commit points to a tree object representing the repository's directory structure and changes
+
+
+Thus magits approach is not efficient for large file sizes 
+
+
+### Branches 
+Git:  They are pointers to specific commits. The branch information is stored in the .git/refs/heads directory, with each branch as a separate file containing the commit hash that it points to.
+
+Magit: Similar to Git, branches are also pointers to commits. In Magit, branch information is stored in the branches directory. 
+
+This has been only done for simplicity, as I am not going to implement remote tracking, tags. 
+
+### Merging 
+
+Right now merging does not take into account for merge conflicts, nor does it fix divergent branches.
+
+
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
 import json
 from datetime import datetime
+import hashlib
+import difflib
+import base64
+
+
 
 class Repository:
     def __init__(self, path):
+        # Initialise the repo, makes folders for objects and branches if doesnt exist
+        # by calling initialise repository
         self.path = path
         self.magit_dir = os.path.join(path, '.magit')
         self.objects_dir = os.path.join(self.magit_dir, 'objects')
         self.branches_dir = os.path.join(self.magit_dir, 'branches')
         self.HEAD = os.path.join(self.magit_dir, 'HEAD')
 
+
+        #create master branch 
+        master_branch_path = os.path.join(self.branches_dir, 'master')
+        with open(master_branch_path, 'w') as master_file:
+            master_file.write('') 
         self.initialize_repository()
 
     def initialize_repository(self):
+        # Only create the .magit file is not exist
         if not os.path.exists(self.magit_dir):
             os.makedirs(self.magit_dir)
             os.makedirs(self.objects_dir)
@@ -21,7 +34,15 @@ class Repository:
                 f.write('ref: refs/heads/master\n')
             print(f"Initialized empty Magit repository in {self.magit_dir}")
 
+
+#Blob and Tree Objects: Git uses blob objects for file contents and tree objects to represent 
+#the directory structure. MAgit does not do that for simplicity.  
     def add(self, filenames):
+        # create  a staged directory, unlike gits index file this is a directory with a collection 
+        # of files rather than a single index file 
+        # doesn’t create separate blob objects for each file. Instead,
+        # it saves the entire content of the file in the staging area 
+        # TODO: is to create blob objects 
         staged_area = os.path.join(self.magit_dir, 'staged')
         if not os.path.exists(staged_area):
             os.makedirs(staged_area)
@@ -31,8 +52,6 @@ class Repository:
             with open(filename, 'rb') as file:
                 content = file.read()
                 file_hash = hashlib.sha1(content).hexdigest()
-
-            # Save the file in the staged area
             file_stage_path = os.path.join(staged_area, file_hash)
             with open(file_stage_path, 'wb') as file_stage:
                 file_stage.write(content)
@@ -40,7 +59,8 @@ class Repository:
             print(f"Added {filename} to the staging area.")
 
 
-   def commit(self, message, author):
+    def commit(self, message, author):
+        # Fix: doesn't check whether there are new changes in the staging area before creating a new commit.
         commit_data = {
             'timestamp': datetime.now().isoformat(),
             'author': author,
@@ -48,55 +68,65 @@ class Repository:
             'parent': self.get_current_commit_hash(),
             'changes': self.get_staged_changes()
         }
-
+        # TODO: only commit if files in staged areas and then remove from staged 
         commit_hash = self.create_object(commit_data)
         self.update_HEAD(commit_hash)
         print(f"Committed with hash {commit_hash}")
-
-
-def get_current_commit_hash(self):
-    if os.path.isfile(self.HEAD):
-        with open(self.HEAD, 'r') as f:
-            ref_line = f.readline().strip()
-            if ref_line.startswith('ref:'):
-                ref_path = os.path.join(self.magit_dir, ref_line.split(':')[1].strip())
-                if os.path.isfile(ref_path):
-                    with open(ref_path, 'r') as ref_file:
-                        return ref_file.read().strip()
-    return None
-
-
-
-def get_staged_changes(self):
-    staged_area = os.path.join(self.magit_dir, 'staged')
-    changes = {}
-    if os.path.exists(staged_area):
+        staged_area = os.path.join(self.magit_dir, 'staged')
         for staged_file in os.listdir(staged_area):
-            staged_file_path = os.path.join(staged_area, staged_file)
-            if os.path.isfile(staged_file_path):
-                with open(staged_file_path, 'rb') as file:
-                    content = file.read()
-                    changes[staged_file] = content
-    return changes
+            os.remove(os.path.join(staged_area, staged_file))
 
 
-def create_object(self, data):
-    commit_hash = hashlib.sha1(json.dumps(data).encode()).hexdigest()
-    commit_path = os.path.join(self.objects_dir, commit_hash)
-    with open(commit_path, 'w') as commit_file:
-        json.dump(data, commit_file)
-    return commit_hash
+    def get_current_commit_hash(self):
+        if os.path.isfile(self.HEAD):
+            with open(self.HEAD, 'r') as f:
+                ref_line = f.readline().strip()
+                if ref_line.startswith('ref:'):
+                    ref_path = os.path.join(self.magit_dir, ref_line.split(':')[1].strip())
+                    if os.path.isfile(ref_path):
+                        with open(ref_path, 'r') as ref_file:
+                            return ref_file.read().strip()
+        return None
 
-def update_HEAD(self, commit_hash):
-    if os.path.isfile(self.HEAD):
-        with open(self.HEAD, 'r') as f:
-            ref_line = f.readline().strip()
-            if ref_line.startswith('ref:'):
-                ref_path = os.path.join(self.magit_dir, ref_line.split(':')[1].strip())
-                with open(ref_path, 'w') as ref_file:
-                    ref_file.write(commit_hash)
 
-   def create_branch(self, branch_name):
+
+    def get_staged_changes(self):
+        staged_area = os.path.join(self.magit_dir, 'staged')
+        changes = {}
+        if os.path.exists(staged_area):
+            for staged_file in os.listdir(staged_area):
+                staged_file_path = os.path.join(staged_area, staged_file)
+                if os.path.isfile(staged_file_path):
+                    with open(staged_file_path, 'rb') as file:
+                        content = file.read()
+                        encoded_content = base64.b64encode(content).decode('utf-8')
+                        changes[staged_file] = encoded_content
+        return changes
+
+
+    def create_object(self, data):
+        print(data)
+        commit_hash = hashlib.sha1(json.dumps(data).encode()).hexdigest()
+        commit_path = os.path.join(self.objects_dir, commit_hash)
+        with open(commit_path, 'w') as commit_file:
+            json.dump(data, commit_file)
+        return commit_hash
+    
+    def update_HEAD(self, commit_hash):
+        if os.path.isfile(self.HEAD):
+            with open(self.HEAD, 'r') as f:
+                ref_line = f.readline().strip()
+                if ref_line.startswith('ref:'):
+                    branch_name = ref_line.split('/')[-1]
+
+                    branch_path = os.path.join(self.branches_dir, branch_name)
+
+                    with open(branch_path, 'w') as branch_file:
+                        branch_file.write(commit_hash)
+
+
+
+    def create_branch(self, branch_name):
         branch_path = os.path.join(self.branches_dir, branch_name)
         current_commit = self.get_current_commit_hash()
         with open(branch_path, 'w') as branch_file:
@@ -104,6 +134,7 @@ def update_HEAD(self, commit_hash):
         print(f"Branch {branch_name} created.")
 
     def switch_branch(self, branch_name):
+        # todo : handle uncommited changes 
         branch_path = os.path.join(self.branches_dir, branch_name)
         if os.path.exists(branch_path):
             with open(self.HEAD, 'w') as head_file:
@@ -114,66 +145,68 @@ def update_HEAD(self, commit_hash):
 
 
 
-def merge(self, source_branch, destination_branch):
-    # a basic merge without conflict 
-    source_branch_path = os.path.join(self.branches_dir, source_branch)
-    destination_branch_path = os.path.join(self.branches_dir, destination_branch)
+    def merge(self, source_branch, destination_branch):
+        # a basic merge ASSUMING NO CONFILICT 
+        source_branch_path = os.path.join(self.branches_dir, source_branch)
+        destination_branch_path = os.path.join(self.branches_dir, destination_branch)
 
-    if os.path.exists(source_branch_path) and os.path.exists(destination_branch_path):
-        with open(source_branch_path, 'r') as source_file:
-            source_commit = source_file.read().strip()
+        if os.path.exists(source_branch_path) and os.path.exists(destination_branch_path):
+            with open(source_branch_path, 'r') as source_file:
+                source_commit = source_file.read().strip()
 
-        with open(destination_branch_path, 'w') as dest_file:
-            dest_file.write(source_commit)
-        
-        print(f"Merged {source_branch} into {destination_branch}")
-    else:
-        print("Branches not found.")
-
-
-def diff(self, commit_hash1, commit_hash2):
-    commit1_path = os.path.join(self.objects_dir, commit_hash1)
-    commit2_path = os.path.join(self.objects_dir, commit_hash2)
-
-    if os.path.exists(commit1_path) and os.path.exists(commit2_path):
-        with open(commit1_path, 'r') as file1, open(commit2_path, 'r') as file2:
-            lines1 = file1.readlines()
-            lines2 = file2.readlines()
-
-            for line in difflib.unified_diff(lines1, lines2, fromfile=commit_hash1, tofile=commit_hash2):
-                print(line)
-    else:
-        print("Commits not found.")
-
-
-
-def status(self):
-    staged_area = os.path.join(self.magit_dir, 'staged')
-    if os.path.exists(staged_area):
-        print("Staged files:")
-        for file in os.listdir(staged_area):
-            print(file)
-    else:
-        print("No files staged for commit.")
-
-
-
-def log(self):
-    # Basic log implementation
-    current_commit = self.get_current_commit_hash()
-    while current_commit:
-        commit_path = os.path.join(self.objects_dir, current_commit)
-        if os.path.exists(commit_path):
-            with open(commit_path, 'r') as commit_file:
-                commit_data = json.load(commit_file)
-                print(f"commit {current_commit}")
-                print(f"Author: {commit_data['author']}")
-                print(f"Date: {commit_data['timestamp']}")
-                print(f"\n\t{commit_data['message']}\n")
-
-            current_commit = commit_data['parent'] if 'parent' in commit_data else None
+            with open(destination_branch_path, 'w') as dest_file:
+                dest_file.write(source_commit)
+            
+            print(f"Merged {source_branch} into {destination_branch}")
         else:
-            break
+            print("Branches not found.")
+
+
+    def diff(self, commit_hash1, commit_hash2):
+        commit1_path = os.path.join(self.objects_dir, commit_hash1)
+        commit2_path = os.path.join(self.objects_dir, commit_hash2)
+
+        if os.path.exists(commit1_path) and os.path.exists(commit2_path):
+            with open(commit1_path, 'r') as file1, open(commit2_path, 'r') as file2:
+                lines1 = file1.readlines()
+                lines2 = file2.readlines()
+
+                for line in difflib.unified_diff(lines1, lines2, fromfile=commit_hash1, tofile=commit_hash2):
+                    print(line)
+        else:
+            print("Commits not found.")
+
+
+
+    def status(self):
+        # just checking if files are staged for commit 
+        # TODO: add the files not staged for commit, and untracked (new) files
+        staged_area = os.path.join(self.magit_dir, 'staged')
+        if os.path.exists(staged_area):
+            print("Staged files:")
+            for file in os.listdir(staged_area):
+                print(file)
+        else:
+            print("No files staged for commit.")
+
+
+
+    def log(self):
+        #basic log reading from objects directory 
+        current_commit = self.get_current_commit_hash()
+        while current_commit:
+            commit_path = os.path.join(self.objects_dir, current_commit)
+            if os.path.exists(commit_path):
+                with open(commit_path, 'r') as commit_file:
+                    commit_data = json.load(commit_file)
+                    print(f"commit {current_commit}")
+                    print(f"Author: {commit_data['author']}")
+                    print(f"Date: {commit_data['timestamp']}")
+                    print(f"\n\t{commit_data['message']}\n")
+                # if parent exists other wise none
+                current_commit = commit_data['parent'] if 'parent' in commit_data else None
+            else:
+                break
 
 
 
@@ -190,4 +223,13 @@ def log(self):
 
 
 # Usage
-repo = Repository('/path/to/your/new/repo')
+repo = Repository('test')
+#repo.add(['test/test1.py'])
+#repo.commit('nice','mai')
+#repo.status()
+#repo.log() 
+#repo.diff("0863aebc3ad229f06867aae724f75d44f12e861c","d368e86ea7f69dcf7c18e8c4f6ec94ac5f6b0ac7")
+#repo.get_staged_changes()
+repo.create_branch("branch1")
+repo.switch_branch("branch1")
+
